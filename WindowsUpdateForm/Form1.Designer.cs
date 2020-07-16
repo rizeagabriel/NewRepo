6cdb707c9ba1819a60c7827a8aa3832b73654ddf
rizeagabriel <grizea82@yahoo.com>
2020-07-16 12:14:23 +0300

    import project

---
+++ WindowsUpdateForm/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsUpdateForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(73, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Workstation Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Myanmar Text", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(73, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Power Adaptor";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(351, 276);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Install";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(459, 276);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.MintCream;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::WindowsUpdateForm.Properties.Resources._250px_Windows_10_Logo_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(80, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(328, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status Device";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(328, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Powered";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Myanmar Text", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(73, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(215, 37);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Internet Connection";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(328, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "ON / OFF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(569, 321);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows 10";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsUpdateForm/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Management;
+using System.IO;
+
+namespace WindowsUpdateForm
+{
+
+    public partial class Form1 : Form
+    {
+        PowerStatus powerStatus = SystemInformation.PowerStatus;
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+        public static bool IsConnectedToInternet()
+        {
+            int Desc;
+            return InternetGetConnectedState(out Desc, 0);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            power();
+        }
+
+        public string WhichONe
+        {
+            get
+            {
+                string connected = SystemInformation.PowerStatus.BatteryChargeStatus == BatteryChargeStatus.NoSystemBattery ? "PC Plugin" : "Laptop On Battery";
+                return connected;
+            }
+        }
+
+        // verificare pc sau laptop
+        public enum ChassisTypes
+        {
+            Other = 1,
+            Unknown,
+            Desktop,
+            LowProfileDesktop,
+            PizzaBox,
+            MiniTower,
+            Tower,
+            Portable,
+            Laptop,
+            Notebook,
+            Handheld,
+            DockingStation,
+            AllInOne,
+            SubNotebook,
+            SpaceSaving,
+            LunchBox,
+            MainSystemChassis,
+            ExpansionChassis,
+            SubChassis,
+            BusExpansionChassis,
+            PeripheralChassis,
+            StorageChassis,
+            RackMountChassis,
+            SealedCasePC
+        }
+
+        public static ChassisTypes GetCurrentChassisType()
+        {
+            ManagementClass systemEnclosures = new ManagementClass("Win32_SystemEnclosure");
+            foreach (ManagementObject obj in systemEnclosures.GetInstances())
+            {
+                foreach (int i in (UInt16[])(obj["ChassisTypes"]))
+                {
+                    if (i > 0 && i <25)
+                    {
+                        return (ChassisTypes)i;
+                    }
+                }
+            }
+            return ChassisTypes.Unknown;
+        }
+
+        // verificare baterie (ac adaptor)
+        private Boolean baterie()
+        {
+            if (powerStatus.PowerLineStatus == PowerLineStatus.Online)
+            {
+                label4.Text = "Plugin On Power "  + Convert.ToString(powerStatus.BatteryLifePercent * 100) + "%";
+                return true;
+            }
+            else
+            {
+                label4.Text = "On Battery " + Convert.ToString(powerStatus.BatteryLifePercent * 100) + "%";
+                return false;
+            }
+        }
+
+        private void power()
+        {
+            label3.Text = GetCurrentChassisType().ToString();
+            baterie();
+            label6.Text = IsConnectedToInternet().ToString();
+
+
+            if (baterie().Equals(true) && IsConnectedToInternet().Equals(true))
+            {
+                btnNext.Enabled = true;
+            }
+        }
+
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            //buton install 
+        }
+
+
+
+    }
+}
